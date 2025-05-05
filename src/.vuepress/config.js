c854ddc47501b34c536491dc7876c15dac0405c1
goegoe0212 <goegoe0212@outlook.jp>
2025-05-05 11:22:17 +0000

    feat: パッケージのバージョンアップ

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,12 +1,15 @@
 import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress'
 import { viteBundler } from '@vuepress/bundler-vite'
+import { searchPlugin } from '@vuepress/plugin-search'
+
 
 export default defineUserConfig({
-  lang: 'en-US',
+  lang: 'ja-JP',
   base: '/bootcamp/',
   title: 'Bootcamp',
-  description: 'Bootcamp Site',
+  description: 'ハンズオン資料集です。',
+
 
   theme: defaultTheme({
     navbar: [
@@ -15,7 +18,15 @@ export default defineUserConfig({
         link: '/',
       },
     ],
+    
+    repo: 'goegoe0212/bootcamp',
   }),
+
+  plugins: [
+    searchPlugin({
+      // options
+    }),
+  ],
 
   bundler: viteBundler(),
 })
