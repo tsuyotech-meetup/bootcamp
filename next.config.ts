a09fb4e7dba851dc4e140062080c1d3fee43c19a
goegoe0212 <goegoe0212@outlook.jp>
2025-05-09 15:32:48 +0000

    feat: サブディレクトリ対応を実施

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+const base = process.env.VUEPRESS_BASE || '';
 
 const nextConfig: NextConfig = {
   /* config options here */
+    output: 'export',
+    assetPrefix: base,
+    basePath: base,
+    trailingSlash: true,
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
